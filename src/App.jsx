ab2989d88c603013b3a9384614196e6831abc475
caohuijun2018 <1160364324@qq.com>
2021-11-12 16:41:16 +0800

    test: 再次测试准入检查

--- src/App.jsx
+++ src/App.jsx
@@ -15,20 +15,20 @@ const App = () => (
             />
           </div>
           <p className="description">
-            Get started by editing <code className="code">src/App.jsx</code>
+            Get started by editing <code className="code">src/App.tsx</code>
           </p>
           <div className="grid">
-            <a href="https://modernjs.dev/docs/start" className="card">
+            <a href="#" className="card">
               <h2>Quick Start</h2>
             </a>
-            <a href="https://modernjs.dev/docs/guides" className="card">
+            <a href="#" className="card">
               <h2>Handbook</h2>
             </a>
-            <a href="https://modernjs.dev/docs/apis" className="card">
+            <a href="#" className="card">
               <h2>API Reference </h2>
             </a>
             <a
-              href="https://modernjs.dev/coming-soon"
+              href="#"
               target="_blank"
               rel="noopener noreferrer"
               className="card">
@@ -37,11 +37,8 @@ const App = () => (
           </div>
         </main>
         <footer className="footer">
-          <a
-            href="https://modernjs.dev"
-            target="_blank"
-            rel="noopener noreferrer">
-            Powered by Modern.js
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            Powered by Modern.JS
           </a>
         </footer>
       </div>
