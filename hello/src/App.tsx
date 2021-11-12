99dc61bf39c7f65255b0a5f89ddbb0ccf7f09657
caohuijun2018 <1160364324@qq.com>
2021-11-12 16:56:33 +0800

    feat: init

--- hello/src/App.tsx
+++ hello/src/App.tsx
@@ -18,17 +18,17 @@ const App = () => (
             Get started by editing <code className="code">src/App.tsx</code>
           </p>
           <div className="grid">
-            <a href="#" className="card">
+            <a href="https://modernjs.dev/docs/start" className="card">
               <h2>Quick Start</h2>
             </a>
-            <a href="#" className="card">
+            <a href="https://modernjs.dev/docs/guides" className="card">
               <h2>Handbook</h2>
             </a>
-            <a href="#" className="card">
+            <a href="https://modernjs.dev/docs/apis" className="card">
               <h2>API Reference </h2>
             </a>
             <a
-              href="#"
+              href="https://modernjs.dev/coming-soon"
               target="_blank"
               rel="noopener noreferrer"
               className="card">
@@ -37,8 +37,11 @@ const App = () => (
           </div>
         </main>
         <footer className="footer">
-          <a href="#" target="_blank" rel="noopener noreferrer">
-            Powered by Modern.JS
+          <a
+            href="https://modernjs.dev"
+            target="_blank"
+            rel="noopener noreferrer">
+            Powered by Modern.js
           </a>
         </footer>
       </div>
