accea697b4f5714fea9797390f20cb6b0383d87f
caohuijun2018 <1160364324@qq.com>
2021-11-15 14:29:34 +0800

    feature:使用Utility Class定制样式

--- hello/src/App.tsx
+++ hello/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { List } from 'antd';
-import LaddaButton, { S, SLIDE_UP } from 'react-ladda';
 import 'ladda/dist/ladda.min.css';
-import Avatar from './components/Avatar';
+import 'tailwindcss/base.css';
+import 'tailwindcss/components.css';
+import 'tailwindcss/utilities.css';
+import Item from './components/Item';
+import './styles/utils.css';
 
 const getAvatar = (users: Array<{ name: string; email: string }>) =>
   users.map(user => ({
@@ -19,40 +22,10 @@ const mockData = getAvatar([
 
 function App() {
   return (
-    // <ul>
-    //   {mockData.map(({ name, avatar, email }) => (
-    //     <li key={name}>
-    //       <img src={avatar} width={60} height={60} /> ---
-    //       <span>{name}</span> ---
-    //       <span>{email}</span>
-    //     </li>
-    //   ))}
-    // </ul>
     <>
       <List
         dataSource={mockData}
-        renderItem={({ name, email, avatar }) => (
-          <List.Item
-            key={name}
-            actions={[
-              <LaddaButton
-                key={email}
-                loading={false}
-                data-size={S}
-                data-style={SLIDE_UP}
-                data-spinner-size={20}
-                data-spinner-color="#ddd"
-                data-spinner-lines={8}>
-                Call
-              </LaddaButton>,
-            ]}>
-            <List.Item.Meta
-              avatar={<Avatar alt="avatar" src={avatar} />}
-              title={name}
-              description={email}
-            />
-          </List.Item>
-        )}
+        renderItem={info => <Item info={info} key={info.name} />}
       />
     </>
   );
