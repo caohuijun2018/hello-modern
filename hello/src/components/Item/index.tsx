29cecca424e1d9dfc0788717f986d197542b9603
caohuijun2018 <1160364324@qq.com>
2021-11-15 15:08:42 +0800

    feature:尝试组件测试

--- hello/src/components/Item/index.tsx
+++ hello/src/components/Item/index.tsx
@@ -20,7 +20,7 @@ const Item = ({ info }: { info: InfoProps }) => {
           type="button"
           disabled={archived}
           className={`bg-blue-500  text-white font-bold
-            py-2 px-4 rounded-full hover:bg-blue-700`}>
+            py-2 px-4 rounded-full hover:bg-blue-700 2xl:bg-red-500`}>
           Archive
         </button>
       </div>
